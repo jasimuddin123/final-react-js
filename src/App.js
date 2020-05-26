344543e610cd95ddef63c3e3fb99c28089b75b61
jasimuddin123 <jasimuddindxn@gmail.com>
2020-05-26 08:54:05 -0700

    JSX, Dynamic content, Dynamic Style in React

--- src/App.js
+++ src/App.js
@@ -3,12 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+const person = {
+  name:"Jasim Uddin",
+  job: "Pakibagi"
+}
+
+const person2 = {
+  name:"Kamal Hossain",
+  job:"Eag Supplier"
+}
+
+const style = {
+  color:'red',
+  backgroundColor:'yellow'
+}
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+  <h2 className="" style={style}>heading title {person.name + " " + person.job}</h2>
+  <h3 style={{backgroundColor:'cyan', color:'blue'}}>Name:{person2.name + " " + person2.job}</h3>
+         <h2>This is Jasim Uddin practice React js</h2>
         </p>
      
       </header>
