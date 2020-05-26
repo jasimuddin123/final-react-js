a87bd1a8f8a4f0d628b48675571b1a7d9c4cdf31
jasimuddin123 <jasimuddindxn@gmail.com>
2020-05-26 09:48:06 -0700

    Create Component, return multiple HTML from a component

--- src/App.js
+++ src/App.js
@@ -3,34 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
-const person = {
-  name:"Jasim Uddin",
-  job: "Pakibagi"
-}
-
-const person2 = {
-  name:"Kamal Hossain",
-  job:"Eag Supplier"
-}
-
-const style = {
-  color:'red',
-  backgroundColor:'yellow'
-}
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-  <h2 className="" style={style}>heading title {person.name + " " + person.job}</h2>
-  <h3 style={{backgroundColor:'cyan', color:'blue'}}>Name:{person2.name + " " + person2.job}</h3>
-         <h2>This is Jasim Uddin practice React js</h2>
-        </p>
-     
+        <p> I am react Person </p>
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
+    
       </header>
     </div>
   );
 }
 
+function Person(){
+  return (
+  <div style={{border:'1px solid green', margin:'20px'}}>
+    <h2>This is Heading two</h2>
+    <h3>This is heading three</h3>
+  </div>
+  )
+}
 export default App;
